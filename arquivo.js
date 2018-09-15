fa3b7f0fa0247bac15bd8ec6f66cd17941b640d7
valerialeticia <43298249+valerialeticia@users.noreply.github.com>
2018-09-15 13:27:39 -0300

    album de figurinhas
    figurinhas

---
+++ arquivo.js
@@ -0,0 +1,9 @@
+function mudar (imagem, titulo, favorito) {
+    
+    document.getElementById('imagem_principal').src = imagem;
+    document.getElementById('titulo').value = titulo;
+    document.getElementById('favorito').value = favorito;
+    
+    
+    
+}
